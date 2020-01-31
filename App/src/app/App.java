67efcc8950df7b5eb497796458df63c7d85fc494
Patton Burge <pburge@basecampcodingacademy.org>
2020-01-31 11:19:41 -0600

    It Now meets Expectations forgot to implement ser

--- App/src/app/App.java
+++ App/src/app/App.java
@@ -11,20 +11,21 @@ import java.util.Scanner;
 public class App {
     static Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<Student> students = loadStudents();
 
-        System.out.println("Good Morning could I get a name please? ");
-        System.out.print(">>> ");
-        String name = in.nextLine();
-        System.out.println(name);
-        System.out.println("What time did you get here? Enter in this format (X.XX) or 0 if you did not show up");
-        System.out.print(">>> ");
-        String time = in.nextLine();
-        System.out.println(time);
-        Student newStudent = new Student(name, time);
+        Student newStudent = startDay();
 
+        students.add(newStudent);
         saveStudents(students);
+    }
+
+    public static Student startDay() {
+        System.out.print("Good Morning could I get a name please:  ");
+        String name = in.nextLine();
+        System.out.print("What time did you get here? Enter in this format (X.XX) or 0 if you did not show up: ");
+        String time = in.nextLine();
+        return new Student(name, time);
     }
 
     private static void saveStudents(ArrayList<Student> students) {
@@ -33,6 +34,7 @@ public class App {
             ObjectOutputStream os = new ObjectOutputStream(fileStream);
             os.writeObject(students);
             os.close();
+
         } catch (IOException ex) {
             System.out.println("Failed to save orders.");
         }
--- App/src/app/Student.java
+++ App/src/app/Student.java
@@ -1,6 +1,9 @@
 package app;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
     String name;
     String time;
 
